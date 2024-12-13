19ba7e0c38efce43469e06eae8864e6ab6d377e2
NOB CODER <m.argareksapati21@gmail.com>
2024-12-14 00:13:25 +0700

    Add files via upload

---
+++ Fundamental/Main.java
@@ -0,0 +1,83 @@
+package Fundamental;
+
+   public class Main // Dcoder di ganti sesuai nama main file
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("NOB CODER HERE, NEW PROJECT");
+    
+    // Tipe tipe data di Java
+    // Integer, Byte, Short, Long, Double, Float, Char, Boolean
+    
+    // 1. Integer (Tipe bilangan bulat standar )
+      int i = 10;
+      System.out.println("=====INTEGER=====");
+      System.out.println("Nilai Integer i = " + (i + 1)); 
+      System.out.println("Nilai Max = " + Integer.MAX_VALUE); 
+      System.out.println("Nilai Min = " + Integer.MIN_VALUE); 
+      System.out.println("Besar Integer = " + Integer.BYTES + "bytes"); 
+      System.out.println("Besar Integer = " + Integer.SIZE + "bit"); 
+      
+     // 2. Byte (Menyimpan nilai bilangan bulat kecil) 
+      int b = 10;
+      System.out.println("=====BYTE=====");
+      System.out.println("Nilai Byte b = " + b);
+      System.out.println("Nilai Max = " + Byte.MAX_VALUE); 
+      System.out.println("Nilai Min = " + Byte.MIN_VALUE); 
+      System.out.println("Besar Integer = " + Byte.BYTES + "bytes"); 
+      System.out.println("Besar Integer = " + Byte.SIZE + "bit"); 
+      
+      // 3. Short (Menyimpan nilai bulat sedang)
+      int s = 10;
+      System.out.println("=====SHORT=====");
+      System.out.println("Nilai Short s = " + s);
+      System.out.println("Nilai Max = " + Short.MAX_VALUE); 
+      System.out.println("Nilai Min = " + Short.MIN_VALUE); 
+      System.out.println("Besar Integer = " + Short.BYTES + "bytes"); 
+      System.out.println("Besar Integer = " + Short.SIZE + "bit"); 
+      
+      // 4. Long (Menyimpan bilangan bulat yang lebih besar) 
+      int l = 10;
+      System.out.println("=====LONG=====");
+      System.out.println("Nilai Long l = " + l);
+      System.out.println("Nilai Max = " + Long.MAX_VALUE); 
+      System.out.println("Nilai Min = " + Long.MIN_VALUE); 
+      System.out.println("Besar Integer = " + Long.BYTES + "bytes"); 
+      System.out.println("Besar Integer = " + Long.SIZE + "bit"); 
+      
+      // 5. Double (Koma atau bilangan real)
+      double d = 10.5d;
+      System.out.println("=====Double=====");
+      System.out.println("Nilai Double d = " + d);
+      System.out.println("Nilai Max = " + Double.MAX_VALUE); 
+      System.out.println("Nilai Min = " + Double.MIN_VALUE); 
+      System.out.println("Besar Integer = " + Double.BYTES + "bytes"); 
+      System.out.println("Besar Integer = " + Double.SIZE + "bit");
+      
+      // 6. Float (Menyimpan angka desimal)
+      int f = 10;
+      System.out.println("=====Float=====");
+      System.out.println("Nilai Float f = " + f);
+      System.out.println("Nilai Max = " + Float.MAX_VALUE); 
+      System.out.println("Nilai Min = " + Float.MIN_VALUE); 
+      System.out.println("Besar Integer = " + Float.BYTES + "bytes"); 
+      System.out.println("Besar Integer = " + Float.SIZE + "bit"); 
+      
+      // 7. Char (Karakter) berdasarkan ASCII
+      char c = 'c';
+      System.out.println("=====CHAR=====");
+      System.out.println("Nilai Char c = " + c);
+      System.out.println("Nilai Max = " + Character.MAX_VALUE); 
+      System.out.println("Nilai Min = " + Character.MIN_VALUE); 
+      System.out.println("Besar Integer = " + Character.BYTES + "bytes"); 
+      System.out.println("Besar Integer = " + Character.SIZE + "bit"); 
+      
+      // Boolean (True or False)
+      boolean val = false;
+      System.out.println("=====INTEGER=====");
+      System.out.println("Nilai Integer i = " + val);
+      System.out.println("Nilai Max = " + Boolean.TRUE); 
+      System.out.println("Nilai Min = " + Boolean.FALSE); 
+      
+   }
+ }
