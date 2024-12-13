60d1a1443b309b639ad9ea41dbcc609bbb491f48
NOB CODER <m.argareksapati21@gmail.com>
2024-12-14 00:18:15 +0700

    Add files via upload

---
+++ OperasiAssigment/Dcoder.java
@@ -0,0 +1,38 @@
+package OperasiAssigment;
+
+   public class Dcoder // Dcoder di ganti sesuai nama main file
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("NOB CODER HERE, NEW PROJECT");
+    
+    // Operator Assisgment
+    
+    // 1. Assignent operator jumlah
+    int a = 1;
+    a += 10; // a = a + 10;
+    System.out.println("nilai a = " + a);
+    
+    // 2. Assignent operator pengurangan
+    int b = 100;
+    b -= 25; // b = b - 25;
+    System.out.println("nilai b = " + b);
+    
+    // 3. Assignent operator perkalian
+    int c = 100;
+    c *= 25; // c = c x 20;
+    System.out.println("nilai c = " + c);
+    
+    // 4. Assignment operator pembagian
+    int d = 10;
+    d /= 7; // d = d : 7;
+    System.out.println("nilai d = " + d);
+    
+    // 5 Assignment operator modulus
+    int e = 10;
+    e %= 7;
+    System.out.println("nilai e = " + e);
+    
+    
+   }
+ }
